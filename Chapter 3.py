71b2131b461f1a04450e2ebf92031c9561f9cab8
sikajipo <30323306+sikajipo@users.noreply.github.com>
2017-07-27 13:00:10 -0400

    Add files via upload

---
+++ Chapter 3.py
@@ -0,0 +1,13 @@
+Python 3.6.2 (v3.6.2:5fd33b5, Jul  8 2017, 04:14:34) [MSC v.1900 32 bit (Intel)] on win32
+Type "copyright", "credits" or "license()" for more information.
+>>> def hello():
+	print('Howdy!')
+	print('Howdy!!!')
+	print('Hello there.')
+
+	
+>>> hello()
+Howdy!
+Howdy!!!
+Hello there.
+>>> 
